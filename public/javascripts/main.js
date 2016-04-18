59c718e175da5f742c9dbd7d61386c6d06548c60
Antoine Ando <antoine.ando@gmail.com>
2016-04-18 13:27:08 +0200

    tesd

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,19 +12,42 @@ $(document).ready(function() {
 
 
 
-/*
+
     $(function(){
         assetList.forEach(function(assetName, key){
             assets[assetName].forEach(function(value, index){
                 src = "images/Assets/" + assetName + "/" + value;
-                console.log(src);
-                image = getDataImg(src);
-                console.log(image);
+
+                function getDataUri(url, callback) {
+                    var image = new Image();
+
+                    image.onload = function () {
+                        var canvas = document.createElement('canvas');
+                        canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+                        canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+
+                        canvas.getContext('2d').drawImage(this, 0, 0);
+
+                        // Get raw image data
+                        callback(canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, ''));
+
+                        // ... or get as Data URI
+                        callback(canvas.toDataURL('image/png'));
+                    };
+
+                    image.src = url;
+                }
+
+                // Usage
+                getDataUri('/logo.png', function(dataUri) {
+                    // Do whatever you'd like with the Data URI!
+                });
+                
 
             });
         });
     });
-    */
+    
     
 /*
     
